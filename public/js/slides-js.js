c082d3048ff55b24dcd4231bb7f453a4e5bea434
lyc <22719973+l-yc@users.noreply.github.com>
2019-10-26 15:52:39 +0800

    ui: added fullscreen presentation support

--- public/js/slides-js.js
+++ public/js/slides-js.js
@@ -1,9 +1,10 @@
 window.onload = function() {
-
     document.getElementById('slide-control-prev').onclick = prevClick;
     document.getElementById('slide-control-next').onclick = nextClick;
+    document.getElementById('slide-control-fullscreen').onclick = presentFullscreen;
 }
 
+/** Slide Playback Controls **/
 let stack = [];
 console.log(slideNumber);
 console.log(animationList);
@@ -55,3 +56,55 @@ $(document).ready(function() {
 });
 
 $(document).on('click', '.slide', nextClick);
+
+/** Other Slide Controls **/
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
+function presentFullscreen() {
+    //var elem = document.querySelector('.slide'); // Make the slide go full screen.
+    var elem = document.body;
+    requestFullScreen(elem);
+}
+
+document.onfullscreenchange = function ( event ) {
+    let sidebar = document.querySelector('.sidebar');
+    let slideControls = document.querySelector('.slide-controls');
+
+    let slide = document.querySelector('.slide');
+
+    if (document.fullscreen) {
+        // hide all the unnecessary stuff
+        sidebar.style.display = 'none';
+        slideControls.style.display = 'none';
+
+        let maxWidth = screen.availWidth,
+            maxHeight = screen.availHeight;
+        if (maxWidth > maxHeight) slide.style.height = maxHeight, slide.style.width  = maxHeight * aspectRatio;
+        else                      slide.style.width  = maxWidth , slide.style.height = maxWidth  / aspectRatio;
+        console.log(maxWidth, maxHeight);
+
+    } else {
+        sidebar.style.display = 'initial';
+        slideControls.style.display = 'initial';
+
+        let maxWidth  = 0.8 * (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth),
+            maxHeight = 0.8 * (window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight);
+        if (maxWidth > maxHeight) slide.style.height = maxHeight, slide.style.width  = maxHeight * aspectRatio;
+        else                      slide.style.width  = maxWidth , slide.style.height = maxWidth  / aspectRatio;
+
+        console.log(maxWidth, maxHeight);
+    }
+};
+
