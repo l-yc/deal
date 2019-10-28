a64b1053c29e674768235a5666d4ab6635bf1d03
lyc <22719973+l-yc@users.noreply.github.com>
2019-10-28 10:34:22 +0800

    slide-selector: auto-detect path separator for compatibility on different os

--- index.js
+++ index.js
@@ -25,14 +25,18 @@ app.get('/browse/view', (req, res) => {
 app.get('/browse/data', (req, res) => {
     let query = url.parse(req.url,true).query;
 
-    let filePath = path.join(appRoot, query.path || "");
+    query.path = query.path || path.sep;
+    let filePath = path.join(appRoot, query.path);
     fs.readdir(filePath, { withFileTypes: true })
         .then(files => {
             //console.log(files);
             files = files
                 .filter(f => { return f.isDirectory() || (f.isFile() && path.extname(f.name) === '.pug') })
                 .map(f => { f.isDirectory = f.isDirectory(); return f });
-            res.json(files);
+            res.json({
+                meta: { sep: path.sep, path: query.path },
+                files: files
+            });
         })
         .catch(err => {
             console.log(err);
